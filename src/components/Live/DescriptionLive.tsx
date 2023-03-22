cea8df38d63008570a0a0b8cd66211ac98799669
JazaelRivas <80536633+JazaelRivas@users.noreply.github.com>
2023-03-22 12:32:51 -0600

    feature: Pentagon

--- src/components/Live/DescriptionLive.tsx
+++ src/components/Live/DescriptionLive.tsx
@@ -2,16 +2,22 @@ import CounterLive from "./CounterLive"
 
 const DescriptionLive = () => {
     return (
-        <div>
-            <h3 className="subtitle">24 de Marzo 2023</h3>
-            <h3 className="subtitle">11:00 AM</h3>
+        <div className="descriptionlive">
 
-            <h1>Title</h1>
-            <p>
-                lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+            <div className="pentagon">
+                <div className="description-content">
+                    <h3 className="subtitle">24 de Marzo 2023</h3>
+                    <h3 className="subtitle">11:00 AM</h3>
 
-            <CounterLive/>
+                    <h1 style={{color:'white'}}>Title</h1>
+                    <p style={{color:'white'}}>
+                        lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+
+                    <CounterLive />
+                </div>
+            </div>
+
         </div>
     )
 }
